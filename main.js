3ee01c9e8edee0a709782aa7894efe9b50e420b2
giselledelgadill <97265938+giselledelgadill@users.noreply.github.com>
2022-05-31 19:52:54 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ lefty=0;
 rightx=0;
 righty=0;
 djleft=0;
+djright=0;
  function preload(){
      song=loadSound("music.mp3");
  }
@@ -19,9 +20,29 @@ function draw(){
     image(video,0,0,600,500);
 fill("blue");
 stroke("orange");
+if(djright>0.2){
+
+
+circle(rightx,righty,20);
+if(righty>0&&righty<=100){
+    document.getElementById("noob").innerHTML="speed= 0.5x";
+    song.rate(0.5);
+}
+else if(righty>100&&righty<=200){
+    document.getElementById("noob").innerHTML="speed= 1x";
+    song.rate(1);
+}
+else if(righty>200&&righty<=300){
+    document.getElementById("noob").innerHTML="speed= 1.5x";
+    song.rate(1.5);
+}
+else if(righty>300&&righty<=400){
+    document.getElementById("noob").innerHTML="speed= 2x";
+    song.rate(2);
+}
+}
+
 if(djleft>0.2){
-
-
 circle(leftx,lefty,20);
 InNumberlefty=Number(lefty);
 remove=floor(InNumberlefty*2);
@@ -42,6 +63,8 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
     console.log(results);
+    djright=results[0].pose.keypoints[9].score;
+    console.log("djright= "+djright);
     djleft=results[0].pose.keypoints[9].score;
     console.log("djleft= "+djleft);
    leftx=results[0].pose.leftWrist.x;
